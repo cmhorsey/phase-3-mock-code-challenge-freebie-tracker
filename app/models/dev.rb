01d389c69733b1cc334ac288d26ccc8363863b67
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-07-22 14:33:51 -0400

    Complete lab

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,14 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
 
+  def received_one?(item_name)
+    freebies.exists?(item_name: item_name)
+  end
+
+  def give_away(dev, freebie)
+    if freebie.dev_id == self.id
+      freebie.update(dev_id: dev.id)
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,23 @@
+Company.destroy_all
+Dev.destroy_all
+Freebie.destroy_all
+
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+google = Company.create(name: "Google", founding_year: 1998)
+facebook = Company.create(name: "Facebook", founding_year: 2004)
+dunder_mifflin = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+enron = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+rick = Dev.create(name: "Rick")
+morty = Dev.create(name: "Morty")
+mr_meseeks = Dev.create(name: "Mr. Meseeks")
+gazorpazop = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
-# ***************************************************************
-# * TODO: create freebies! Remember, a freebie belongs to a dev *
-# * and a freebie belongs to a company.                         *
-# ***************************************************************
-# Create freebies Here
-
-puts "Seeding done!"
+mug = Freebie.create(item_name: "Mug", value: 15, company: google, dev: rick)
+shirt = Freebie.create(item_name: "Shirt", value: 20, company: facebook, dev: morty)
+notebook = Freebie.create(item_name: "Notebook", value: 10, company: dunder_mifflin, dev: mr_meseeks)
+pen = Freebie.create(item_name: "Pen", value: 5, company: enron, dev: gazorpazop)
+sticker = Freebie.create(item_name: "Sticker", value: 5, company: google, dev: gazorpazop)
